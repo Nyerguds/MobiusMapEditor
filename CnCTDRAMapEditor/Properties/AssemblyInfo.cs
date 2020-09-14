093e743cd8cc10f99ef526cda50963d889448ff2
Rampastring <rampastring@outlook.com>
2020-09-14 14:54:46 +0300

    Update AssemblyInfo

--- CnCTDRAMapEditor/Properties/AssemblyInfo.cs
+++ CnCTDRAMapEditor/Properties/AssemblyInfo.cs
@@ -19,12 +19,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CnC TDRA Map Editor")]
+[assembly: AssemblyTitle("C&C Tiberian Dawn and Red Alert Map Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Electronic Arts Inc. & Rampastring")]
+[assembly: AssemblyCompany("Rampastring")]
 [assembly: AssemblyProduct("CnC TDRA Map Editor")]
-[assembly: AssemblyCopyright("© 2020 Electronic Arts Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Command & Conquer and the original map editor © 2020 Electronic Arts Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
