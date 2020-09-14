be5fc59880e2b540b09d82e56af40e07638573fb
Rampastring <rampastring@outlook.com>
2020-09-14 18:53:23 +0300

    Fix compile errors in Steamworks.NET code

--- CnCTDRAMapEditor/Steamworks.NET/types/SteamClient/SteamAPI_PostAPIResultInProcess_t.cs
+++ CnCTDRAMapEditor/Steamworks.NET/types/SteamClient/SteamAPI_PostAPIResultInProcess_t.cs
@@ -1,5 +1,2 @@
-
-#endif // !DISABLESTEAMWORKS
-
 // This file is no longer needed. Valve has removed the functionality.
 // We continue to generate this file to provide a small amount of backwards compatability.
